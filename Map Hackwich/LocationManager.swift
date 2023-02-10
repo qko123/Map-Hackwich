541e9f632da8f2070f1e17f4a0a143a42044dab7
Quinn Kozak <quinn.kozak@icloud.com>
2023-02-10 09:34:13 -0600

    Added geocoder

--- Map Hackwich/LocationManager.swift
+++ Map Hackwich/LocationManager.swift
@@ -9,9 +9,8 @@ import Foundation
 import CoreLocation
 
 class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
-    
     var locationManager = CLLocationManager()
-    
+    var geocoder = CLGeocoder()
     override init() {
         super.init()
         locationManager.delegate = self
